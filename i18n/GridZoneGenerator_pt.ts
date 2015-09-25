8a055511e3e0ca662ccf889a7abb5619d08f0a84
lcoandrade <lcoandrade@gmail.com>
2015-09-25 11:31:52 -0300

    New translation

--- i18n/GridZoneGenerator_pt.ts
+++ i18n/GridZoneGenerator_pt.ts
@@ -17,7 +17,7 @@
 <context>
     <name>GridZoneGeneratorDialog</name>
     <message>
-        <location filename="grid_zone_generator_dialog.py" line="279"/>
+        <location filename="grid_zone_generator_dialog.py" line="285"/>
         <source>Warning!</source>
         <translation>Aviso!</translation>
     </message>
@@ -57,19 +57,24 @@
         <translation>Um arquivo de saída deve ser especificado!</translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog.py" line="265"/>
+        <location filename="grid_zone_generator_dialog.py" line="271"/>
         <source>Generating grid, please wait...</source>
         <translation>Gerando grid, por favor espere...</translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog.py" line="297"/>
+        <location filename="grid_zone_generator_dialog.py" line="300"/>
         <source>Success!</source>
         <translation>Sucesso!</translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog.py" line="297"/>
+        <location filename="grid_zone_generator_dialog.py" line="300"/>
         <source>Grid Created successfully!</source>
         <translation>Grid Criado com sucesso!</translation>
+    </message>
+    <message>
+        <location filename="grid_zone_generator_dialog.py" line="294"/>
+        <source>Grid Zones</source>
+        <translation>Enquadramento</translation>
     </message>
 </context>
 <context>
@@ -80,109 +85,114 @@
         <translation>Gerador de Enquadramento Sistemático</translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="47"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="62"/>
         <source>Desired Grid Zone Scale: </source>
         <translation>Escala de Enquadramento desejada: </translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="147"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="162"/>
         <source>1000k</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="152"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="167"/>
         <source>500k</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="157"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="172"/>
         <source>250k</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="162"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="177"/>
         <source>100k</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="167"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="182"/>
         <source>50k</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="172"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="187"/>
         <source>25k</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="177"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="192"/>
         <source>10k</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="182"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="197"/>
         <source>5k</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="187"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="202"/>
         <source>2k</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="192"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="207"/>
         <source>1k</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="112"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="127"/>
         <source>CRS: </source>
         <translation></translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="122"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="137"/>
         <source>Select CRS</source>
         <translation>Selecione o CRS</translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="131"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="146"/>
         <source>Reference Data</source>
         <translation>Dados de referência</translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="139"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="154"/>
         <source>Base Scale: </source>
         <translation>Escala Base: </translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="202"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="217"/>
         <source>Index Frame Selection</source>
         <translation>Seleção de Índice de Nomenclatura</translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="210"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="225"/>
         <source>MIR</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="228"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="243"/>
         <source>MI</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="246"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="261"/>
         <source>INDEX</source>
         <translation>INOM</translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="22"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="34"/>
         <source>Destination File</source>
         <translation>Destino do Arquivo</translation>
     </message>
     <message>
-        <location filename="grid_zone_generator_dialog_base.ui" line="36"/>
+        <location filename="grid_zone_generator_dialog_base.ui" line="48"/>
         <source>Save File</source>
         <translation>Salvar Arquivo</translation>
+    </message>
+    <message>
+        <location filename="grid_zone_generator_dialog_base.ui" line="20"/>
+        <source>Create shapefile layer? (Instead of temporary layer)</source>
+        <translation>Criar camada shapefile? (Ao invés de uma camada temporária)</translation>
     </message>
 </context>
 </TS>
